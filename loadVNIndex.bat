c839ecb17cd890d291423b45894725551d400fc1
ngocdev <ngocdvt@gmail.com>
2026-01-16 20:29:34 +0900

    .bat

--- loadVNIndex.bat
+++ loadVNIndex.bat
@@ -1,4 +1,4 @@
 @echo off
-cd /d "D:\1_Programming\2_Python\VNIndexAutoscrap"
-"C:\Users\A22M\AppData\Local\Programs\Python\Python313\python.exe" "D:\1_Programming\2_Python\VNIndexAutoscrap\loadVNIndex.py"
+cd /d "D:\1_Programming\1_Python\VNIndexAutoscrap"
+"C:\Users\A22M\AppData\Local\Programs\Python\Python313\python.exe" "D:\1_Programming\1_Python\VNIndexAutoscrap\loadVNIndex.py"
 pause
